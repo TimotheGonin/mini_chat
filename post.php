e97854c2493fc5fb69b7de91e07ad73d3f9b2f0d
Timothé Gonin <gonin.timothe@gmail.com>
2021-03-30 16:07:50 +0200

    send msg + succes

--- post.php
+++ post.php
@@ -1,4 +1,31 @@
 <?php
-    //REDIRECTION
-    header('Location: index.php');
+    //BDD CONNECTION
+    try 
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } 
+    catch (Exception $e) 
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    //VERIFICATION
+    if(isset($_POST['pseudo_msg']) && isset($_POST['message_msg']))
+    {
+        $pseudo = $_POST['pseudo_msg'];
+        $txt = $_POST['message_msg'];
+
+        // INSERT PREP REQ
+        $req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES(?, ?)');
+        $req->execute(array($_POST['pseudo_msg'], $_POST['message_msg']));
+        header('Location: index.php?succes=true');
+        exit();
+    } 
+    else 
+    {
+        //REDIRECTION
+        header('Location: index.php');
+        exit();
+    }
+
 ?>
